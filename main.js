cc6903d63417ed4c0795c042478ea015037354a2
YMandarin <46614277+YMandarin@users.noreply.github.com>
2019-10-31 08:37:08 +0100

    html update

--- main.js
+++ main.js
@@ -3,7 +3,39 @@ const path = require("path");
 const url = require("url");
 
 let win;
+let onMac = false;
 
+//create first Window
 function createWindow(){
-    win = new BrowserWindow({width:1200,height:720});   
+    if(process.platform === "darwin"){
+        onMac = true;
+    }
+
+    win = new BrowserWindow({width:1200,height:720});
+
+    win.loadURL(url.format({
+        pathname: path.join(__dirname,"pages/index.html"),
+        protocol: "file",
+        slashes: true
+    }));
+
+    win.on("closed", ()=>{
+        win = null;
+    });
+
+    openDevTools();
+
 }
+
+//open devTools of Chromium
+function openDevTools(){
+    win.webContents.openDevTools();
+}
+
+app.on("ready",createWindow);
+
+app.on("window-all-closed",()=>{
+    if (onMac){
+        app.quit();
+    }
+});
